cd6ba332a69cd9e7529524e3d8d6e3f7623575f7
steveen Ordoñez <steveen.ordonez@manticore-labs.com>
2023-05-24 19:17:44 -0500

    feat:db:v.0.0.1

--- database/migrations/2023_03_01_262553_create_locations_table.php
+++ database/migrations/2023_03_01_262553_create_locations_table.php
@@ -19,6 +19,13 @@ return new class extends Migration
             $table->string('city');
             $table->string('canton');
             $table->string('parish');
+            $table->string('postal_code');
+            $table->string('description');
+            $table->integer('people')->unsigned()->nullable();
+            $table->foreign('people')->references('id')->on('people');
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->timestamps();
         });
     }
 
