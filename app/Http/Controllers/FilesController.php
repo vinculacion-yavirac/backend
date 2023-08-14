3b71c732429565aad7808bbfcf755ca990c26a25
hscordonez <zh311505@gmail.com>
2023-08-14 16:02:18 -0500

    feat: api file

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -5,10 +5,72 @@ namespace App\Http\Controllers;
 use App\Models\File;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * @OA\Schema(
+ *     schema="File",
+ *     title="File",
+ *     description="File model",
+ *     @OA\Property(property="id", type="integer", format="int64"),
+ *     @OA\Property(property="name", type="string", maxLength=100),
+ *     @OA\Property(property="type", type="string", maxLength=200),
+ *     @OA\Property(property="content", type="string"),
+ *     @OA\Property(property="observation", type="string", maxLength=200),
+ *     @OA\Property(property="state", type="boolean", default=true),
+ *     @OA\Property(property="size", type="integer"),
+ *     @OA\Property(property="briefcase_id", type="integer", nullable=true),
+ *     @OA\Property(property="document_id", type="integer", nullable=true),
+ *     @OA\Property(property="created_at", type="string", format="date-time"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time"),
+ * )
+ */
 class FilesController extends Controller
 {
 
-
+    /**
+     * @OA\Get(
+     *     path="/api/files",
+     *     summary="Obtener lista de todos los archivos",
+     *     operationId="getFiles",
+     *     tags={"Files"},
+     *     security={{"bearer":{}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Respuesta exitosa",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="data", type="object",
+     *                 @OA\Property(property="files", type="array", @OA\Items(ref="#/components/schemas/File"))
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="No autorizado",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="No autorizado.")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="No encontrado",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Recurso no encontrado.")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error interno del servidor",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="message", type="string", example="Ocurrió un error en el servidor."),
+     *             @OA\Property(property="error", type="string", example="Mensaje de error detallado.")
+     *         )
+     *     )
+     * )
+     */
     public function getFiles()
     {
         $files = File::all();
